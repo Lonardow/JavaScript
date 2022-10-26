b24c6961d5cfc25e1f81c503126184e0958b274b
Leonardo Ferreira Guimarães <lonardow_fg@hotmail.com>
2022-10-26 08:12:27 -0300

    mais estudo e projeto

---
+++ loops/doWhile/doWhile.js
@@ -0,0 +1,9 @@
+let x = 11;
+
+do {
+  //comando
+  document.write(x + "<br />");
+
+  //break
+  //continue
+} while (x <= 10);
---
+++ loops/while/while.js
@@ -0,0 +1,13 @@
+let x = 1;
+
+while (x <= 10) {
+  document.write(x + "<br />");
+
+  if (x === 4) {
+    break;
+  }
+
+  x++;
+}
+
+document.write("fim");
